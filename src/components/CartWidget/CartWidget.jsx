e5931adfd01ae386d54d39875446ee22cbe196c2
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2023-06-06 00:10:48 -0300

    add sel-falign numero carrito

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,7 +4,7 @@ const CartWidget = () => {
   
     const imgCarrito = "https://icones.pro/wp-content/uploads/2021/05/icone-de-panier-jaune.png"
     return (
-    <div>
+    <div className='d-flex align-items-end'>
         <img className='imgCarrito' src={imgCarrito} alt="Carrito" />
         <strong>10</strong>
     </div>
