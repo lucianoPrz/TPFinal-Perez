d95695190f797076a102ec525c3dd305b1586181
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2023-06-06 10:11:31 -0300

    modificacion style li navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import './NavBar.css'
 const NavBar = () => {
     return (
         <header>
-            <nav id='botonera'  className="navbar navbar-expand-lg">
+            <nav id='botonera' className="navbar navbar-dark navbar-expand-lg">
                 <div className="container-fluid">
                     <h1>Ind FC</h1>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,17 +13,17 @@ const NavBar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav m-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                Camisetas
-                            </li>
-                            <li className="nav-item active">
-                                Camperas
+                                <strong className='linkNav'> Camisetas </strong>
                             </li>
                             <li className="nav-item">
-                                Buzos
+                                <strong className='linkNav'> Camperas </strong>
+                            </li>
+                            <li className="nav-item">
+                                <strong className='linkNav'> Buzos </strong>
                             </li>
                         </ul>
                     </div>
-                    <CartWidget/>
+                    <CartWidget />
                 </div>
             </nav>
 
