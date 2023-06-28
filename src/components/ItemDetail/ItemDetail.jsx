7ea17c1dd97589e361db7d9c670eb995a6c3c90c
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2023-06-28 11:49:12 -0300

    se aplica estiloa itemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 const ItemDetail = ({id, nombre, precio, img}) => {
   return (
-    <div>
-        <h2>Nombre: {nombre}</h2>
-        <h3>Precio: {precio}</h3>
-        <h3>ID: {id}</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt animi dolores culpa iure suscipit aliquam dolore natus voluptatum fuga eius magni, illo eos dolorum et fugiat. Itaque inventore quo dolorum?</p>
-        <img src={img} alt={nombre} />
+    <div className='card w-75 d-flex container-fluid m-3'>
+      <div className="card-body d-flex flex-column">
+        <h2 className='card-title'>Nombre: {nombre}</h2>
+        <h3 className='card-title'>Precio: $ {precio}</h3>
+        <h3 className='card-title'>ID: {id}</h3>
+        <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt animi dolores culpa iure suscipit aliquam dolore natus voluptatum fuga eius magni, illo eos dolorum et fugiat. Itaque inventore quo dolorum?</p>
+      </div>
+      <img src={img} alt={nombre} className='img card-img-top w-50 m-3' />
     </div>
   )
 }
