8ba28ab3870f8912e03f54a2f40c9897c635216e
Sergio Luciano Pérez <sluciano.perez@gamil.com>
2023-07-22 00:04:03 -0300

    agrego descripcion por parametro

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ const App = () => {
         <CarritoProvider>
           <NavBar />
           <Routes>
-            <Route path="/" element={<ItemListContainer gretting={"Mayorista de camisetas e indumentaria"} />} />
-            <Route path="/categoria/:idCategoria" element={<ItemListContainer gretting={"Mayorista de camisetas e indumentaria"} />} />
+            <Route path="/" element={<ItemListContainer gretting={"Arma tu equipo"} />} />
+            <Route path="/categoria/:idCategoria" element={<ItemListContainer gretting={"Arma tu equipo"} />} />
             <Route path="/item/:idItem" element={< ItemDetailContainer />} />
             <Route path="/cart" element={<Cart/>} />
             <Route path="/checkout" element={<Checkout/>} />
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,35 +4,38 @@ import ItemCount from "../ItemCount/ItemCount"
 import { useContext } from "react"
 import { CarritoContext } from "../../context/CarritoContext"
 
-const ItemDetail = ({id, nombre, precio, img, stock}) => {
+const ItemDetail = ({ id, nombre, precio, img, stock, descripcion }) => {
   const [agregarCantidad, setAgregarCantidad] = useState(0);
 
-  const {agregarProducto} = useContext(CarritoContext);
+  const { agregarProducto } = useContext(CarritoContext);
 
   const handlerCantidad = (cantidad) => {
     setAgregarCantidad(cantidad)
-    
-    const item = {id, nombre, precio};
+
+    const item = { id, nombre, precio };
     agregarProducto(item, cantidad)
 
   };
 
 
   return (
-    <div className='card w-75 d-flex container-fluid m-3'>
-      <div className="card-body d-flex flex-column">
-        <h2 className='card-title'>Nombre: {nombre}</h2>
-        <h3 className='card-title'>Precio: $ {precio}</h3>
-        <h3 className='card-title'>ID: {id}</h3>
-        <p className='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt animi dolores culpa iure suscipit aliquam dolore natus voluptatum fuga eius magni, illo eos dolorum et fugiat. Itaque inventore quo dolorum?</p>
+    <div >
+      <div className='card w-75 d-flex container-fluid my-3 '>
+        <div className="card-body d-flex flex-column">
+          <h2 className='card-title'>Nombre: {nombre}</h2>
+          <h3 className='card-title'>Precio: $ {precio}</h3>
+          <h3 className='card-title'>ID: {id}</h3>
+          <p className='card-text fw-semibold'>{descripcion}</p>
+          <p className="card-subtitle mb-2 text-muted">Talles únicos. Compra minima 5 unidades.</p>
+        </div>
+        <img src={img} alt={nombre} className='img card-img-top w-50 m-3' />
+        {
+          agregarCantidad > 0 ? (<div>
+            <Link to="/cart" className="ms-3 btn btn-success"> Terminar Compra </Link>
+            <Link to="/" className="ms-3 btn btn-primary"> Seguir comprando </Link>
+          </div>) : (<ItemCount inicial={5} stock={stock} onAdd={handlerCantidad} />)
+        }
       </div>
-      <img src={img} alt={nombre} className='img card-img-top w-50 m-3' />
-      {
-        agregarCantidad > 0 ? (<div>
-          <Link to="/cart" className="ms-3 btn btn-success"> Terminar Compra </Link>
-          <Link to="/" className="ms-3 btn btn-primary"> Seguir comprando </Link>
-          </div> ) : (<ItemCount inicial={1} stock={stock} onAdd={handlerCantidad} /> )
-      }
     </div>
   )
 }
